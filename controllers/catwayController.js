30181a6939cdb8e2812dc0494ce0892b1a059da2
Lydie-pixel <fleursdesiles7@gmail.com>
2026-02-19 15:30:34 +0000

    Remise en place de toutes les autres pages suite au bugs

---
+++ controllers/catwayController.js
@@ -0,0 +1,66 @@
+const catwayService = require("../services/catwayService");
+
+// GET ALL
+exports.getAllCatways = async (req, res) => {
+  try {
+    const catways = await catwayService.getAll();
+    res.json(catways);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// GET ONE
+exports.getCatwayById = async (req, res) => {
+  try {
+    const catway = await catwayService.getById(req.params.id);
+
+    if (!catway) {
+      return res.status(404).json({ message: "Catway introuvable" });
+    }
+
+    res.json(catway);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// POST
+exports.createCatway = async (req, res) => {
+  try {
+    const catway = await catwayService.create(req.body);
+    res.status(201).json(catway);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// PUT
+exports.updateCatway = async (req, res) => {
+  try {
+    const catway = await catwayService.update(req.params.id, req.body);
+
+    if (!catway) {
+      return res.status(404).json({ message: "Catway introuvable" });
+    }
+
+    res.json(catway);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// DELETE
+exports.deleteCatway = async (req, res) => {
+  try {
+    const catway = await catwayService.delete(req.params.id);
+
+    if (!catway) {
+      return res.status(404).json({ message: "Catway introuvable" });
+    }
+
+    res.status(204).send();
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
